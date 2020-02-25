eeb8431e3d6f1e71eb17fbf82abcffec69dc31af
kekich <kek@cheburek>
2020-02-25 16:57:31 +0200

    Final touch

--- login_script.sh
+++ login_script.sh
@@ -3,3 +3,5 @@
 $(aws ecr get-login --no-include-email --region eu-central-1)
 docker pull 760836743460.dkr.ecr.eu-central-1.amazonaws.com/toxictypo
 docker run -d -p 80:8080 760836743460.dkr.ecr.eu-central-1.amazonaws.com/toxictypo
+echo "-------------------------"
+echo "Up and Running :)"
