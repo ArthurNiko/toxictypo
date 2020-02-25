ea46792b026008bdd9b2540626a07c0530e6d98a
kekich <kek@cheburek>
2020-02-25 16:28:36 +0200

    login script fix

--- login_script.sh
+++ login_script.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-aws ecr get-login-password | docker login --username AWS --password-stdin 760836743460.dkr.ecr.eu-central-1.amazonaws.com
-docker run -p 80:8080 760836743460.dkr.ecr.eu-central-1.amazonaws.com/toxictypo
+aws ecr get-login-password | docker login --username AWS --password-stdin 760836743460.dkr.ecr.eu-central-1.amazonaws.com docker run -p 80:8080 760836743460.dkr.ecr.eu-central-1.amazonaws.com/toxictypo
