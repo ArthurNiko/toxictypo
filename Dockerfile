07ae88a0aebe8be852e46bc331b7eda501029b36
kekich <kek@cheburek>
2020-02-25 16:51:02 +0200

    Final touch

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,4 @@
 FROM openjdk:8-jre-alpine
 WORKDIR /app
 COPY ./target .
-ENTRYPOINT [ "java","-jar" ]
-CMD [ "toxictypoapp-1.0-SNAPSHOT.jar" ]
+ENTRYPOINT [ "/bin/sh", "-c", "java -jar toxictypoapp*" ]
--- login_script.sh
+++ login_script.sh
@@ -2,4 +2,4 @@
 
 $(aws ecr get-login --no-include-email --region eu-central-1)
 docker pull 760836743460.dkr.ecr.eu-central-1.amazonaws.com/toxictypo
-docker run -p 80:8080 760836743460.dkr.ecr.eu-central-1.amazonaws.com/toxictypo
+docker run -d -p 80:8080 760836743460.dkr.ecr.eu-central-1.amazonaws.com/toxictypo
